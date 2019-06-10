708c0969d41e19dd8f473dc062f4eb18d3d26d9a
nzbenji <benlittle53@gmail.com>
2019-06-10 19:53:24 +1200

    Grabbing lat & lng coords from google api

--- src/js/modules/search.js
+++ src/js/modules/search.js
@@ -5,6 +5,8 @@ const $searchInput = document.querySelector('.search__input');
 const $searchCity = document.querySelector('.search__city');
 const $searchWrapper = document.querySelector('.search-wrapper');
 
+const GEOCODE_KEY = 'AIzaSyDKYunPGZOTx70U-G5bj8oxjQvluZBDBeA';
+
 export const initSearch = () => {
     listeners();
 }
@@ -16,11 +18,21 @@ const listeners = () => {
         $searchInput.focus();
 
         if($searchInput.value === '') return;
-        
+
         location = $searchInput.value;
         $searchInput.value = '';
+        getLatLng(location);
         render();
     });
+}
+
+const getLatLng = async query => {
+    const reqLink = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_KEY}`
+    const data = await fetch(reqLink);
+    const parsed = await data.json();
+    
+    const Latitude = parsed.results[0].geometry.location.lat;
+    const Longitude = parsed.results[0].geometry.location.lng;
 }
 
 const render = () => {
